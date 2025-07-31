221e2c1a0092d73255b39e8ef83ecb743d6ba323
Yurcheeg <pandorasboxer11@gmail.com>
2025-07-31 12:27:32 +0300

    Add cursor with changing colors on click held/stopped

---
+++ Assets/_GAME/_Scripts/Gameplay/MouseCursor.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseCursor : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    public Vector2 GetPosition() => transform.position;
+
+    private void SetPosition() => transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+    private void Awake() => _spriteRenderer = GetComponent<SpriteRenderer>();
+
+    private void Update()
+    {
+        SetPosition();
+
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+            _spriteRenderer.color = Color.green;
+        if (Mouse.current.leftButton.wasReleasedThisFrame)
+            _spriteRenderer.color = Color.blue;
+    }
+}
